324a3749576a3955939601ffeb6519734e214301
Ahmad sutrio prihatinoto <ahmadsutrio210@gmail.com>
2021-06-01 08:41:48 +0700

    Pemograman_Lanjutan_Algoritma

---
+++ src/Table/RunSorting.java
@@ -0,0 +1,19 @@
+package Table;
+
+import javax.swing.*;
+
+public class RunSorting {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ignored) {
+        }
+        JFrame jFrame = new JFrame("Form Data With Tabel");
+        jFrame.setContentPane(new FormTable().getRootPanel());
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setSize(500, 400);
+        jFrame.setVisible(true);
+    }
+}
+
